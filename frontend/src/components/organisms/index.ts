fd44aff8b9bc2db0a696da0ebbced82a56419a21
Ytemiloluwa <ytemiloluwa@gmail.com>
2026-08-08 20:04:23 +0100

    refactor(ui): add screen and template layers

--- frontend/src/components/organisms/index.ts
+++ frontend/src/components/organisms/index.ts
@@ -1,7 +1,5 @@
 export { AppHeader } from './AppHeader';
 export type { AppHeaderProps } from './AppHeader';
-export { DashboardShell } from './DashboardShell';
-export type { DashboardShellProps } from './DashboardShell';
 export { DataTable } from './DataTable';
 export type { DataTableColumn, DataTableProps } from './DataTable';
 export { NavigationSidebar } from './NavigationSidebar';
